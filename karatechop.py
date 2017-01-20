e72bea9a7b207ca3e5be35116bb652c481a5aa14
pawelaglowacki <pawelglowackicapgemini@gmail.com>
2017-01-20 23:49:05 +0300

    last test fails

--- karatechop.py
+++ karatechop.py
@@ -5,19 +5,19 @@ class Karate:
 	def chop(self, toFind, array):
 
 		arrayLeftBoundary, arrayRightBoundary, arrayMiddle = 0, len(array)-1, len(array)/2
-		while (arrayLeftBoundary <= arrayRightBoundary) :
-			
+		
+		while(arrayLeftBoundary < arrayRightBoundary):
+			if toFind < array[arrayMiddle]:
+				arrayRightBoundary = arrayRightBoundary - arrayMiddle
+				arrayMiddle = (arrayLeftBoundary + arrayRightBoundary)/2						
 			if array[arrayMiddle] < toFind:
-				arrayLeftBoundary = arrayMiddle
-				arrayMiddle = (arrayMiddle+arrayRightBoundary)/2
-			
-			if array[arrayMiddle] > toFind:
-				arrayRightBoundary = arrayMiddle
-				arrayMiddle = (arrayMiddle+arrayLeftBoundary)/2		
-
-			print str(arrayMiddle) + ' ' + str(array[arrayMiddle]) + ' ' + str(toFind)
-			if array[arrayMiddle]  == toFind:
+				arrayLeftBoundary = arrayLeftBoundary + arrayMiddle
+				arrayMiddle = (arrayLeftBoundary + arrayRightBoundary)/2						
+			if array[arrayMiddle] == toFind:
 				return arrayMiddle
+
+		if array[arrayMiddle] == toFind:
+			return arrayMiddle
 		return -1
 
 
--- tests.py
+++ tests.py
@@ -12,6 +12,8 @@ class TestKarateChop(unittest.TestCase):
 		self.assertEqual(1, self.karate.chop(2, [1,2]))
 		self.assertEqual(2, self.karate.chop(3, [1,2,3,4]))
 		self.assertEqual(3, self.karate.chop(4, [1,2,3,4]))
+		self.assertEqual(5, self.karate.chop(12, [1,2,3,7,10,12,14,1200]))
+		self.assertEqual(5, self.karate.chop(12, [1,2,3,7,10,12,14,1200, 1300]))
 		
 
 if __name__ == '__main__':
